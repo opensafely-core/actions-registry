8141893d521d34066046f36b3a6b57f3182af4a3
CarolineMorton <carolinemorton@gmx.com>
2021-07-12 16:02:28 +0100

    add html templates, change views to route to there and display actions

---
+++ registry/actions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'actions'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:action_id>', views.detail, name='detail')
+    # path('<action>/')
+]
