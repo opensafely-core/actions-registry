43318070067ff9808b2e9c2cb5e22f8b755b6e50
CarolineMorton <carolinemorton@gmx.com>
2021-07-12 17:10:28 +0100

    copy from output explorer relevant initial html pages

--- registry/registry/settings.py
+++ registry/registry/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "actions.apps.ActionsConfig",
+    # 'django_vite',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
