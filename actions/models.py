e0610190427c6229548994834315aca30fa0dd84
CarolineMorton <carolinemorton@gmx.com>
2021-07-14 14:02:09 +0100

    change db model and do migrations

--- actions/models.py
+++ actions/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Action(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    type = models.CharField(max_length=50)
-    pub_date = models.DateTimeField("date published")
+    org = models.CharField(max_length=200)
+    repo_name = models.CharField(max_length=200)
+    readme = models.CharField(max_length=2000)
+    about = models.CharField(max_length=400)
 
     def __str__(self):
         return self.name
