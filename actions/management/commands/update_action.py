9e0eeed02efcc669fceb1f864583dda8e8128111
CarolineMorton <carolinemorton@gmx.com>
2021-07-19 14:42:16 +0100

    apply tailwind to readme

--- actions/management/commands/update_action.py
+++ actions/management/commands/update_action.py
@@ -1,3 +1,4 @@
+import pycmarkgfm
 from django.core.management.base import BaseCommand
 
 from actions.github import GithubClient
@@ -25,6 +26,17 @@ class Command(BaseCommand):
         # get contents
         contents = repo.get_contents(path="docs/README.md", ref="main")
         readme = contents.decoded_content
-        action.readme = readme
+        html_readme = pycmarkgfm.gfm_to_html(readme)
+
+        html_readme = html_readme.replace(
+            "<h3>", '<h3 class="text-sm font-semibold text-gray-600"> '
+        )
+        html_readme = html_readme.replace(
+            "<p>", '<p class="mt-1 text-sm text-gray-900"> '
+        )
+        html_readme = html_readme.replace("<h5>", '<h5 class="text-sm font-semibold"> ')
+        html_readme = html_readme.replace("<ul>", '<div class="list-disc">')
+
+        action.readme = html_readme
 
         action.save()
