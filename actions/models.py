7ae9c239f0e7b09fd8ebdae8548d1a9fb89ffa51
CarolineMorton <carolinemorton@gmx.com>
2021-07-20 16:42:29 +0100

    add second table and migrate

--- actions/models.py
+++ actions/models.py
@@ -5,7 +5,6 @@ class Action(models.Model):
     name = models.CharField(max_length=200)
     org = models.CharField(max_length=200)
     repo_name = models.CharField(max_length=200)
-    readme = models.TextField()
     about = models.TextField()
 
     def __str__(self):
@@ -13,3 +12,16 @@ class Action(models.Model):
 
     class Meta:
         unique_together = ["org", "repo_name"]
+
+
+class Version(models.Model):
+    action = models.ForeignKey(Action, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=100)
+    date = models.DateField()
+    readme = models.TextField()
+
+    def __str__(self):
+        return f"{self.action} - {self.tag}"
+
+    class Meta:
+        unique_together = ["action", "tag"]  # might be action_id
