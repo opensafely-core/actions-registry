4dcfd0eb255befc970a8989c4b425548073491cc
CarolineMorton <carolinemorton@gmx.com>
2021-07-22 17:41:25 +0100

    update html and views

--- actions/views.py
+++ actions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Action, Version
+from .models import Action
 
 
 def index(request):
@@ -10,7 +10,7 @@ def index(request):
 
 def detail(request, action_id):
     action = get_object_or_404(Action, pk=action_id)
-    version = get_object_or_404(Version, action_id=action.id)
+    version = action.get_latest_version()
     readme = version.readme
 
     return render(
