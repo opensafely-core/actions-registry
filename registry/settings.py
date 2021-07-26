6cd2df3c5bbee2377e399165d68c9f00c6878c8f
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-07-26 16:04:56 +0100

    Integrate Django and Vite

--- registry/settings.py
+++ registry/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+
 from environs import Env
 
 
@@ -38,6 +39,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "django_vite",
     "actions.apps.ActionsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -127,10 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "assets", "dist"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
+DJANGO_VITE_ASSETS_PATH = "/static/"
+DJANGO_VITE_DEV_MODE = env.bool("DJANGO_VITE_DEV_MODE", default=False)
+DJANGO_VITE_MANIFEST_PATH = os.path.join(BASE_DIR, "staticfiles", "manifest.json")
 
 # Insert Whitenoise Middleware.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
