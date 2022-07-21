65286d3279bb1d6c77c39ad8c02c4c11469963fd
George Hickman <george@ghickman.co.uk>
2022-07-21 15:42:05 +0100

    Add X-XSS-Protection header

--- actions/settings.py
+++ actions/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "actions.middleware.XSSFilteringMiddleware",
 ]
 
 ROOT_URLCONF = "actions.urls"
