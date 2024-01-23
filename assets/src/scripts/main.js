467e1afe5aa069326f18059058fa9a7e88c8ef7e
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-01-23 14:35:14 +0000

    Remove sass dependency and use CSS for styles

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -3,8 +3,11 @@ import "@fontsource/public-sans/400.css";
 import "@fontsource/public-sans/500.css";
 import "@fontsource/public-sans/600.css";
 import "@fontsource/public-sans/700.css";
-import "../styles/main.scss";
 
+// Styles
+import "../styles/main.css";
+
+// Plausible
 if (document.location.hostname === "actions.opensafely.org") {
   const script = document.createElement("script");
   script.defer = true;
