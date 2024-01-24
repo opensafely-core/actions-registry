220044208e870716314fa9dc389caa33756bb93d
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-01-24 09:25:36 +0000

    Update Tailwind typography styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
-import typography from "@tailwindcss/typography";
+import tailwindTypography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -24,7 +24,45 @@ export default {
           900: "#001936",
         },
       },
+      typography: ({ theme }) => ({
+        oxford: {
+          css: {
+            "--tw-prose-body": theme("colors.gray[800]"),
+            "--tw-prose-headings": theme("colors.oxford[900]"),
+            "--tw-prose-lead": theme("colors.oxford[700]"),
+            "--tw-prose-links": theme("colors.oxford[600]"),
+            "--tw-prose-bold": theme("colors.oxford[900]"),
+            "--tw-prose-counters": theme("colors.oxford[600]"),
+            "--tw-prose-bullets": theme("colors.gray[800]"),
+            "--tw-prose-hr": theme("colors.oxford[300]"),
+            "--tw-prose-quotes": theme("colors.oxford[900]"),
+            "--tw-prose-quote-borders": theme("colors.oxford[300]"),
+            "--tw-prose-captions": theme("colors.oxford[700]"),
+            "--tw-prose-code": theme("colors.oxford[900]"),
+            "--tw-prose-pre-code": theme("colors.oxford[100]"),
+            "--tw-prose-pre-bg": theme("colors.oxford[900]"),
+            "--tw-prose-th-borders": theme("colors.oxford[300]"),
+            "--tw-prose-td-borders": theme("colors.oxford[200]"),
+            "--tw-prose-invert-body": theme("colors.oxford[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.oxford[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.oxford[400]"),
+            "--tw-prose-invert-bullets": theme("colors.oxford[600]"),
+            "--tw-prose-invert-hr": theme("colors.oxford[700]"),
+            "--tw-prose-invert-quotes": theme("colors.oxford[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.oxford[700]"),
+            "--tw-prose-invert-captions": theme("colors.oxford[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.oxford[300]"),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme("colors.oxford[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.oxford[700]"),
+          },
+        },
+      }),
     },
   },
-  plugins: [typography],
+  plugins: [tailwindTypography],
 }
