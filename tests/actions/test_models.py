facf591f765f4548efabff008b8fcc791aaf8fad
Alice Wong <alice.wong@thedatalab.org>
2025-02-26 16:50:34 +0000

    Order versions from latest to oldest
    - We will be making a change to show all past versions of an action to the user.
    - To display the action versions from latest to oldest,
    a sort is needed either database-side or application-side.
    - As of current plans, we would only retrieve versions from the
    database to get the latest version or display them in reverse-
    chronological order.
    - Given that a retrieval is always coupled
    to a sort, we can neatly specify the ordering in the `Meta`
    class of the `Version` model.
    - A corresponding database migration is done, and a unit test
    is added to ensure that the commit timestamp is used for ordering.

--- tests/actions/test_models.py
+++ tests/actions/test_models.py
@@ -14,6 +14,27 @@ def action():
     )
 
 
+def test_versions_are_ordered_from_latest_to_earliest(action):
+    v1_0 = Version.objects.create(
+        action=action,
+        tag="v1.0",
+        committed_at=datetime(2023, 1, 1, tzinfo=timezone.utc),
+    )
+    v1_1 = Version.objects.create(
+        action=action,
+        tag="v1.1",
+        committed_at=datetime(2024, 1, 1, tzinfo=timezone.utc),
+    )
+    v1_2 = Version.objects.create(
+        action=action,
+        tag="v1.2",
+        committed_at=datetime(2025, 1, 1, tzinfo=timezone.utc),
+    )
+    versions = action.versions.all()
+
+    assert list(versions) == [v1_2, v1_1, v1_0]
+
+
 def test_get_latest_version(action):
     # Create a version and check get_latest_version() get this
     Version.objects.create(
