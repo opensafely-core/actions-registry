9ec551a64376188c94557822d272949672b0f289
Alice Wong <alice.wong@thedatalab.org>
2025-03-03 11:35:20 +0000

    Refactor test_models.py
    - This is done to improve readability of the tests, since we anticipate
    more unit tests will be added with subsequent changes to the `Action`
    and `Version` models.
    - Move the creation of a dummy `Action` object to a pytest fixture, and
    reuse it to eliminate duplicate code
    - Remove unnecessary assignment of names to dummy `Version` objects by
    using `Version.objects.create`
    - Also use specific datetimes for `committed_at` instead of
    datetime.now() to be more explicit

--- tests/actions/test_models.py
+++ tests/actions/test_models.py
@@ -1,51 +1,47 @@
 from datetime import datetime, timezone
 
+import pytest
+
 from actions.models import Action, Version
 
 
-def test_get_latest_version():
-    # Create an action
-    action = Action(
+@pytest.fixture
+def action():
+    return Action.objects.create(
         org="opensafely-actions",
         repo_name="test-action",
         about="Testing action",
     )
-    action.save()
 
+
+def test_get_latest_version(action):
     # Create a version and check get_latest_version() get this
-    version = Version(
+    Version.objects.create(
         action=action,
         tag="v1.0",
-        committed_at=datetime.now(timezone.utc),
+        committed_at=datetime(2024, 1, 1, tzinfo=timezone.utc),
         readme="first version",
     )
-    version.save()
     latest_version = action.get_latest_version()
     assert latest_version.tag == "v1.0"
     assert latest_version.readme == "first version"
 
     # Create a second version and check get_latest_version() get this more up to date version
-    version2 = Version(
+    Version.objects.create(
         action=action,
         tag="v1.1",
-        committed_at=datetime.now(timezone.utc),
+        committed_at=datetime(2025, 1, 1, tzinfo=timezone.utc),
         readme="second version",
     )
-    version2.save()
     latest_version = action.get_latest_version()
     assert latest_version.tag == "v1.1"
     assert latest_version.readme == "second version"
 
 
-def test_str():
-    action = Action.objects.create(
-        org="opensafely-actions",
-        repo_name="test-action",
-        about="Testing action",
-    )
+def test_str(action):
     version = action.versions.create(
         tag="v1.0",
-        committed_at=datetime.now(timezone.utc),
+        committed_at=datetime(2024, 1, 1, tzinfo=timezone.utc),
         readme="first version",
     )
 
