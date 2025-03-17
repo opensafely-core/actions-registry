c797bf7b0d171064f1916ae0ba9b90e2753fc2fd
Iain Dillingham <iain@dillingham.me.uk>
2025-03-17 13:22:19 +0000

    Break-out function
    Doing so will help us resolve multiple attributes.

--- actions/utils.py
+++ actions/utils.py
@@ -5,8 +5,13 @@ from bs4 import BeautifulSoup
 
 def resolve_relative_urls_to_absolute(html, base_url, attribute):
     soup = BeautifulSoup(html, "html.parser")
-    for element in soup.find_all(lambda element: element.has_attr(attribute)):
-        url = element.get(attribute)
-        if url and not urlparse(url).netloc:  # is relative
-            element[attribute] = urljoin(base_url, url)
+
+    def resolve():
+        for element in soup.find_all(lambda element: element.has_attr(attribute)):
+            url = element.get(attribute)
+            if url and not urlparse(url).netloc:  # is relative
+                element[attribute] = urljoin(base_url, url)
+
+    resolve()
+
     return str(soup)
